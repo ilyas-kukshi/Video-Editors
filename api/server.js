12c930912e1d81cd600608a992d2dff8669897c6
ilyas-kukshi <ilyaskukshiwala53@gmail.com>
2023-05-28 02:34:07 +0530

    Add signup post call

--- api/server.js
+++ api/server.js
@@ -1,13 +1,59 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const app = express()
 const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Wassup World!')
 })
 
+const bcrypt = require("bcrypt");
+const User = require('../api/models/user_signin')
+app.post("/signup", (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                return res.status(409).json({
+                    message: "Mail exists"
+                });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
+                        console.log(err);
+
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const user = new User({
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user.save()
+                            .then(result => {
+                                console.log(result);
+                                res.status(201).json({
+                                    message: "User created"
+                                });
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                                });
+                            });
+                    }
+                });
+            }
+        });
+});
 
 
 mongoose.connect("mongodb+srv://ilyaskukshi:ilyaskukshi@cluster0.bn4aw.mongodb.net/Video-Editors?retryWrites=true&w=majority")
