e4158d27ebb0e84e08553338e231af1f9a19cbe1
ilyas-kukshi <ilyaskukshiwala53@gmail.com>
2023-05-29 10:51:31 +0530

    Add toasts

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,11 +1,13 @@
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
+import 'package:video_editors/models/reponse_model.dart';
 import 'package:video_editors/models/signin/signin_model.dart';
 import 'package:video_editors/shared/constants.dart';
 
 class AuthServices {
   String url = Constants.baseUrl;
 
-  singinService(SigninModel model) async {
+  Future<ResponseModel?> singinService(SigninModel model) async {
     String finalUrl = "${url}signup";
     print(finalUrl);
     try {
@@ -16,21 +18,23 @@ class AuthServices {
       );
 
       if (response.statusCode == 200) {
-        print('response.body');
+        return ResponseModel(response.statusCode, response.body);
 
         // responseData = response.body;
       } else {
         // Handle error response
+        Fluttertoast.showToast(msg: response.body);
         print('POST request failed with status: ${response.statusCode}');
         print('Response body: ${response.body}');
       }
     } catch (e) {
       // Handle any exceptions that occur during the request
-      print('Error occurred during POST request: $e');
+      Fluttertoast.showToast(msg: e.toString());
     }
+    return null;
   }
 
-  loginService(SigninModel model) async {
+  Future<ResponseModel?> loginService(SigninModel model) async {
     String finalUrl = "${url}login";
     try {
       final response = await http.post(
@@ -40,7 +44,7 @@ class AuthServices {
       );
 
       if (response.statusCode == 200) {
-        print(response.body);
+        return ResponseModel(response.statusCode, response.body);
 
         // responseData = response.body;
       } else {
@@ -52,5 +56,6 @@ class AuthServices {
       // Handle any exceptions that occur during the request
       print('Error occurred during POST request: $e');
     }
+    return null;
   }
 }
