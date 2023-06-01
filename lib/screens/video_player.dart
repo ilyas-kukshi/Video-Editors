99b372127188596ff107cc71edd6be6e2363bcdf
ilyas-kukshi <ilyaskukshiwala53@gmail.com>
2023-06-01 19:00:22 +0530

    Add yt_video_player package and screen

---
+++ lib/screens/video_player.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+class VideoPlayer extends StatefulWidget {
+  String videoId;
+  VideoPlayer({super.key, required this.videoId});
+
+  @override
+  State<VideoPlayer> createState() => _VideoPlayerState();
+}
+
+class _VideoPlayerState extends State<VideoPlayer> {
+  late YoutubePlayerController _controller;
+  // late PlayerState _playerState;
+  // late YoutubeMetaData _videoMetaData;
+  // double _volume = 100;
+  // bool _muted = false;
+  // bool _isPlayerReady = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = YoutubePlayerController(
+      initialVideoId: widget.videoId,
+      flags: const YoutubePlayerFlags(
+        autoPlay: false,
+        mute: false,
+      ),
+    );
+  }
+
+  @override
+  void deactivate() {
+    // Pauses video while navigating to next page.
+    _controller.pause();
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return YoutubePlayerBuilder(
+      player: YoutubePlayer(
+        controller: _controller,
+      ),
+      builder: (context, player) {
+        return Column(
+          children: [
+            player,
+          ],
+        );
+      },
+      // child: YoutubePlayer(
+      //   controller: _controller,
+      //   showVideoProgressIndicator: true,
+      //   // videoProgressIndicatorColor: Colors.amber,
+      // ),
+    );
+  }
+}
